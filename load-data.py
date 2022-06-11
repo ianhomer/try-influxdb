ee6604d74d1430ccbe7b43c30ea14e5ddc325e0e
Ian Homer <ian@local>
2022-06-11 08:07:44 +0100

    feat: refactor to allow experiments

--- load-data.py
+++ load-data.py
@@ -1,39 +1,25 @@
 #!/usr/bin/env python3
 
-import datetime
-import os
 
-from influxdb_client import InfluxDBClient, Point, WritePrecision
+from influxdb_client import InfluxDBClient
 from influxdb_client.client.write_api import SYNCHRONOUS
 
+
+import helper
+from timer import Timer
+from config import Config
+
+timer = Timer()
+config = Config()
+
 print("loading data into influx")
-
-token = os.environ.get("INFLUX_TOKEN_MY_ORGANISATION", "")
-if token == "":
-    raise Exception("Please set environment variable INFLUX_TOKEN_MY_ORGANISATION")
-
-organisation = "my-organisation"
-bucket = "my-bucket"
-
-
-def createPoint(i: int):
-    return (
-        Point("measure")
-        .tag("source", "load-data")
-        .field("value", i)
-        .time(
-            datetime.datetime.utcnow() + datetime.timedelta(milliseconds=i * 100),
-            WritePrecision.NS,
-        )
-    )
-
-
 with InfluxDBClient(
-    url="http://localhost:8086", token=token, org=organisation
+    url="http://localhost:8086", token=config.token, org=config.organisation
 ) as client:
     write_api = client.write_api(write_options=SYNCHRONOUS)
 
-    for i in range(10):
-        point = createPoint(i)
-        write_api.write(bucket, organisation, point)
-        print(f"Loaded {point}")
+    for i in range(1000):
+        point = helper.createPoint(i)
+        timer.start()
+        write_api.write(config.bucket, config.organisation, point)
+        timer.stop()
