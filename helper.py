cde9cb04cc593c1186d6c0b8448105145d37c8e9
Ian Homer <ian@local>
2022-06-12 15:17:45 +0100

    feat: show bucket count

--- helper.py
+++ helper.py
@@ -1,5 +1,7 @@
-from influxdb_client import Point, WritePrecision
+from influxdb_client import InfluxDBClient, Point, WritePrecision
 import datetime
+
+from config import Config
 
 start = datetime.datetime.now()
 
@@ -14,3 +16,17 @@ def createPoint(i: int):
         .field("value", i + 0.123)
         .time(time, WritePrecision.NS)
     )
+
+
+def showBucketInfo():
+    config = Config()
+    with InfluxDBClient(
+        url=config.url, token=config.token, org=config.organisation
+    ) as client:
+        tables = client.query_api().query(
+            f'from(bucket:"{config.bucket}") '
+            + "|> range(start: -1d) "
+            + '|> filter(fn: (r) => r["_measurement"] == "measure") '
+            + "|> count()"
+        )
+        print(f'point count = {tables[0].records[0]["_value"]}')
--- load-data-async-outer.py
+++ load-data-async-outer.py
@@ -41,3 +41,4 @@ async def main():
 
 
 asyncio.run(main())
+helper.showBucketInfo()
