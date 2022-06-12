e5080f5ba094ab5ba7eec1569bbbaea2aa29ba37
Ian Homer <code@ianhomer.com>
2022-06-12 15:37:09 +0100

    feat: total rate

--- timer.py
+++ timer.py
@@ -2,6 +2,8 @@ import itertools
 import time
 import threading
 
+from rx import start
+
 
 class Timer:
     def __init__(self):
@@ -15,9 +17,10 @@ class Timer:
         self.lastPrepTime = 0
         self.lastSentTime = 0
 
-    def getRate(self, current, previous):
+    def getRate(self, current, previous, count):
+
         return (
-            int(self.outputCadence * 1000 / (current - previous))
+            int(count * 1000 / (current - previous))
             if previous > 0 and previous != current
             else 0
         )
@@ -28,16 +31,18 @@ class Timer:
 
     def finish(self):
         finishTime = round(time.time() * 1000)
+        total = next(self.completed)
+        rate = self.getRate(finishTime, self.startTime, total)
         print(
             f"{self.threadName:12} : "
-            + f" end  : {finishTime - self.startTime}ms"
+            + f" end  : {total} in {finishTime - self.startTime}ms : rate {rate}/s"
         )
 
     def prep(self):
         i = next(self.started)
         if i % self.outputCadence == 0:
             prepTime = round(time.time() * 1000)
-            rate = self.getRate(prepTime, self.lastPrepTime)
+            rate = self.getRate(prepTime, self.lastPrepTime, self.outputCadence)
             print(
                 f"{self.threadName:12} : "
                 + f"     {i} : {prepTime - self.startTime}ms : rate {rate}/s"
@@ -48,7 +53,7 @@ class Timer:
         i = next(self.completed)
         if i % self.outputCadence == 0:
             stopTime = round(time.time() * 1000)
-            rate = self.getRate(stopTime, self.lastStopTime)
+            rate = self.getRate(stopTime, self.lastStopTime, self.outputCadence)
             print(
                 f"{self.threadName:12} : "
                 + f"done {i} : {stopTime - self.startTime}ms : rate {rate}/s"
@@ -59,7 +64,7 @@ class Timer:
         i = next(self.sent)
         if i % self.outputCadence == 0:
             sentTime = round(time.time() * 1000)
-            rate = self.getRate(sentTime, self.lastSentTime)
+            rate = self.getRate(sentTime, self.lastSentTime, self.outputCadence)
             print(
                 f"{self.threadName:12} : "
                 + f"send {i} : {sentTime - self.startTime}ms : rate {rate}/s"
